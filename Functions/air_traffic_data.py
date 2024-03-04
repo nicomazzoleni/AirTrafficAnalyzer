b849efd24431cd0aaf8a9cb45abd1e2d9cb35568
robertomue <61682@novasbe.pt>
2024-03-04 12:35:39 +0000

    Created Notebook showcase file & included 4. method in the class

--- Functions/air_traffic_data.py
+++ Functions/air_traffic_data.py
@@ -48,3 +48,35 @@ class AirTrafficData:
             coords2['Longitude'], coords2['Latitude']
         )
 
+    def most_used_airplane_models(self, N, country=None):
+        """
+        Retrieve the N most frequently used airplane models based on the number of routes they operate.
+
+        Parameters:
+        - N (int): The number of airplane models to retrieve.
+        - country (str or list of str, optional): A string or a list of country names to filter the data. 
+          If provided, only routes from the specified countries will be considered. 
+          If None (default), data from all countries will be included.
+
+        Returns:
+        pandas.Series: A Series containing the counts of routes for each airplane model, 
+        indexed by the airplane model name, sorted in descending order of route counts.
+        """
+        airports = self.airports_df[["Airport ID", "Country", "IATA"]]
+        routes = self.routes_df[["Source airport ID", "Destination airport ID"]]
+        airplanes = self.airplanes_df[["Name","IATA code"]]
+
+        #Joining dataframes & data cleaning
+        airports.loc[:, "Airport ID"] = airports["Airport ID"].astype(str)
+        routes_adv = routes.join(airports.set_index("Airport ID"), on ="Source airport ID")
+        data = routes_adv.join(airplanes.set_index("IATA code"), on ="IATA")
+        data = data[(data["Name"].notna()) & (data["IATA"] != "\\N")]
+
+        
+        if country != None:
+             if isinstance(country, str):
+                 country = [country]  
+             return data[data["Country"].isin(country)].groupby("Name").size().nlargest(N)
+        else:
+             return data.groupby("Name").size().nlargest(N)
+
