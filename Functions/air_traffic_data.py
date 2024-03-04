76df74db2f069e7a0f58b3cf52776f50c41cc3c3
Nicolò Mazzoleni <nico@MacBook-Air-di-Nicolo-2.local>
2024-03-04 16:45:25 +0000

    Nico's Branch

--- Functions/air_traffic_data.py
+++ Functions/air_traffic_data.py
@@ -77,6 +77,44 @@ class AirTrafficData:
         
         return data["Distance"].hist() 
 
+    def plot_airports_by_country(self, country_name):
+        """ 
+        Takes a country as an input and plots a map 
+        with the locations of its airports (as well as a map for that country)
+
+        Parameters: 
+        - country_name: Name of the country to plot 
+
+        Returns: 
+        A map of the country's boarders with red dots indicating the airports
+        """
+    
+        # Filter the airports dataframe for the specified country
+        country_airports = self.airports_df[self.airports_df['Country'].str.lower() == country_name.lower()]
+
+        # Check if any airports are found for the country
+        if country_airports.empty:
+            return f"No airports found for the country: {country_name}. Please check the country name and try again."
+
+        # Plotting setup
+        fig, ax = plt.subplots(figsize=(10, 8))
+        ax = plt.axes(projection=ccrs.PlateCarree())
+
+        lats = country_airports['Latitude'].values
+        longs = country_airports['Longitude'].values
+
+        # Create a Cartopy map of the world
+        ax.add_feature(cfeature.COASTLINE)
+        ax.add_feature(cfeature.BORDERS)
+        ax.add_feature(cfeature.LAND, color='lightgray')
+        ax.set_extent([min(longs) - 5, max(longs) + 5, min(lats) - 5, max(lats) + 5])
+
+        # Plot airports
+        ax.scatter(longs, lats, transform=ccrs.PlateCarree(), color='red', marker='o', zorder=5)
+
+        plt.title(f"Airports in {country_name}")
+        plt.show()
+    
     def most_used_airplane_models(self, N, country=None):
         """
         Retrieve the N most frequently used airplane models based on the number of routes they operate.
